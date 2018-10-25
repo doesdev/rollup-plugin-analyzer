9fe861ad360f74ee7f1ffbeeaaa9775c5ae4c6dc
Andrew Carpenter <andrew@audioinhd.com>
2018-10-24 23:37:48 -0400

    Remove stupid console log

--- module.js
+++ module.js
@@ -132,7 +132,6 @@ export const plugin = (opts = {}) => {
   let onAnalysis = (analysis) => {
     if (typeof opts.onAnalysis === 'function') opts.onAnalysis(analysis)
     if (typeof opts.htmlReportPath === 'string') {
-      console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!MADEITHERE!!!!!!1111111111111')
       let rptProm = writeHtmlReport(analysis.modules, opts.htmlReportPath)
       if (opts.onHtmlReport) {
         rptProm.then((rp) => opts.onHtmlReport())
